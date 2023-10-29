2e03dcfab8cad8a2dad3b78eb7b111c2e790a815
f-deniyi <holarfemilekan049@gmail.com>
2023-10-29 11:12:03 -0700

    wallet management

---
+++ src/modules/usersWallet/controller/get_user_balance.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
+import AxiosWithToken from "../../../constants/api_management/MyHttpHelperWithToken";
+
+const UseGetUserBalance = ({ enabled = true, userId }) => {
+    const [lastRefetchTime, setLastRefetchTime] = useState(null);
+
+    const { data, refetch, isLoading } = useQuery(["user_balance", userId], async () => {
+        try {
+            const [response] = [await AxiosWithToken.get(`/wallet/balance/${userId}`)];
+            console.log(response.status);
+            return response.data;
+        } catch (error) {
+            console.log(error.response.data);
+            throw new Error(`Sorry: ${error.response.data.message}`);
+        }
+    }, {
+        enabled,
+        refetchOnWindowFocus: false,
+        onSettled: (data, error) => {
+            if (data) {
+                const now = new Date();
+                setLastRefetchTime(now);
+            }
+
+        }
+
+    });
+
+    return {
+        balance: data?.data?.wallet ?? {},
+        lastRefetchTime,
+        refetch: refetch,
+        isLoading: isLoading,
+    };
+};
+
+export default UseGetUserBalance;
--- src/modules/wallet/component/Transactions.js
+++ src/modules/wallet/component/Transactions.js
@@ -5,18 +5,8 @@ import useGetTransactionsHistoryQuery from "../controller/get_transactions_histo
 import PaginationRounded from '../../../generalComponents/Pagination'
 import moment from 'moment';
 
-const TransactionsTable = () => {
-    const [activePage, setActivePage] = useState(1)
+const TransactionsTable = ({ transactions, pagination, handlePage }) => {
 
-    const { transactions, pagination } = useGetTransactionsHistoryQuery({
-        enabled: true,
-        page: activePage
-
-    })
-
-    const handlePage = (page) => {
-        setActivePage(page)
-    }
 
     return (
         <>
@@ -44,7 +34,7 @@ const TransactionsTable = () => {
                                         {moment(el?.createdAt).format('ll')}
 
                                     </td>
-                                    <td className="p-2 w-[300px] text-[12px] font-medium">{`N${el?.amount}`}</td>
+                                    <td className="p-2 w-[100px] text-[12px] font-medium">{`N${el?.amount?.toLocaleString()}`}</td>
                                     <td className="p-2 w-[150px] text-[12px] font-medium">
                                         <div className="flex items-center justify-center w-full h-full">
                                             <p className={`${el?.confirmed ? 'bg-brandGreen' : 'bg-[#FFA800]'} px-2 py-1 rounded-full text-center text-white text-[10px] font-medium`}>
@@ -54,225 +44,17 @@ const TransactionsTable = () => {
                                             </p>
                                         </div>
                                     </td>
-                                    <td className=" p-2 w-[150px] relative text-[12px] font-medium text-right">
+                                    {/* <td className=" p-2 w-[150px] relative text-[12px] font-medium text-right">
                                         <div>
                                             <button className="flex items-center justify-center text-blue-600 w-full h-full">
                                                 <img src={menu} alt="icon" />
                                             </button>
                                         </div>
-                                    </td>
+                                    </td> */}
                                 </tr>
                             )
                         }
-                        {/* <tr
-                            className="  rounded-lg p-5 relative mb-2 "
-                        >
-                            <td className="p-2 w-[400px] ">
-                                <div className="flex items-center">
-                                    <img
-                                        className="w-[50px] h-[50px] rounded-lg mr-[10px]"
-                                        src={withdrawIcon}
-                                        alt="Item Image"
-                                    />
-                                    <div>
-                                        <p className="text-[12px] font-medium">Deposited Funds</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td className="p-2 w-[300px] text-[12px] font-medium">
-                                7 Aug 2023
 
-                            </td>
-                            <td className="p-2 w-[300px] text-[12px] font-medium">N23,000</td>
-                            <td className="p-2 w-[150px] text-[12px] font-medium">
-                                <div className="flex items-center justify-center w-full h-full">
-                                    <p className="bg-[#FFA800] px-2 py-1 rounded-full text-center text-white text-[10px] font-medium">
-                                        Pending
-                                    </p>
-                                </div>
-                            </td>
-                            <td className=" p-2 w-[150px] relative text-[12px] font-medium text-right">
-                                <div>
-                                    <button className="flex items-center justify-center text-blue-600 w-full h-full">
-                                        <img src={menu} alt="icon" />
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr
-                            className="  rounded-lg p-5 relative mb-2 "
-                        >
-                            <td className="p-2 w-[400px] ">
-                                <div className="flex items-center">
-                                    <img
-                                        className="w-[50px] h-[50px] rounded-lg mr-[10px]"
-                                        src={depositIcon}
-                                        alt="Item Image"
-                                    />
-                                    <div>
-                                        <p className="text-[12px] font-medium">Withdraw Funds</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td className="p-2 w-[300px] text-[12px] font-medium">
-                                7 Aug 2023
-
-                            </td>
-                            <td className="p-2 w-[300px] text-[12px] font-medium">N23,000</td>
-                            <td className="p-2 w-[150px] text-[12px] font-medium">
-                                <div className="flex items-center justify-center w-full h-full">
-                                    <p className="bg-[#FFA800] px-2 py-1 rounded-full text-center text-white text-[10px] font-medium">
-                                        Pending
-                                    </p>
-                                </div>
-                            </td>
-                            <td className=" p-2 w-[150px] relative text-[12px] font-medium text-center">
-                                <div>
-                                    <button className="flex items-center justify-center text-blue-600 w-full h-full">
-                                        <img src={menu} alt="icon" />
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr
-                            className="  rounded-lg p-5 relative mb-2 "
-                        >
-                            <td className="p-2 w-[400px] ">
-                                <div className="flex items-center">
-                                    <img
-                                        className="w-[50px] h-[50px] rounded-lg mr-[10px]"
-                                        src={withdrawIcon}
-                                        alt="Item Image"
-                                    />
-                                    <div>
-                                        <p className="text-[12px] font-medium">Deposited Funds</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td className="p-2 w-[300px] text-[12px] font-medium">
-                                7 Aug 2023
-
-                            </td>
-                            <td className="p-2 w-[300px] text-[12px] font-medium">N23,000</td>
-                            <td className="p-2 w-[150px] text-[12px] font-medium">
-                                <div className="flex items-center justify-center w-full h-full">
-                                    <p className="bg-[#FFA800] px-2 py-1 rounded-full text-center text-white text-[10px] font-medium">
-                                        Pending
-                                    </p>
-                                </div>
-                            </td>
-                            <td className=" p-2 w-[150px] relative text-[12px] font-medium text-center">
-                                <div>
-                                    <button className="flex items-center justify-center text-blue-600 w-full h-full">
-                                        <img src={menu} alt="icon" />
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr
-                            className="  rounded-lg p-5 relative mb-2 "
-                        >
-                            <td className="p-2 w-[400px] ">
-                                <div className="flex items-center">
-                                    <img
-                                        className="w-[50px] h-[50px] rounded-lg mr-[10px]"
-                                        src={depositIcon}
-                                        alt="Item Image"
-                                    />
-                                    <div>
-                                        <p className="text-[12px] font-medium">Withdraw Funds</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td className="p-2 w-[300px] text-[12px] font-medium">
-                                7 Aug 2023
-
-                            </td>
-                            <td className="p-2 w-[300px] text-[12px] font-medium">N23,000</td>
-                            <td className="p-2 w-[150px] text-[12px] font-medium">
-                                <div className="flex items-center justify-center w-full h-full">
-                                    <p className="bg-[#FFA800] px-2 py-1 rounded-full text-center text-white text-[10px] font-medium">
-                                        Pending
-                                    </p>
-                                </div>
-                            </td>
-                            <td className=" p-2 w-[150px] relative text-[12px] font-medium text-center">
-                                <div>
-                                    <button className="flex items-center justify-center text-blue-600 w-full h-full">
-                                        <img src={menu} alt="icon" />
-                                    </button>
-                                </div>
-                            </td>
-                        </tr><tr
-                            className="  rounded-lg p-5 relative mb-2 "
-                        >
-                            <td className="p-2 w-[400px] ">
-                                <div className="flex items-center">
-                                    <img
-                                        className="w-[50px] h-[50px] rounded-lg mr-[10px]"
-                                        src={withdrawIcon}
-                                        alt="Item Image"
-                                    />
-                                    <div>
-                                        <p className="text-[12px] font-medium">Deposited Funds</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td className="p-2 w-[300px] text-[12px] font-medium">
-                                7 Aug 2023
-
-                            </td>
-                            <td className="p-2 w-[300px] text-[12px] font-medium">N23,000</td>
-                            <td className="p-2 w-[150px] text-[12px] font-medium">
-                                <div className="flex items-center justify-center w-full h-full">
-                                    <p className="bg-[#FFA800] px-2 py-1 rounded-full text-center text-white text-[10px] font-medium">
-                                        Pending
-                                    </p>
-                                </div>
-                            </td>
-                            <td className=" p-2 w-[150px] relative text-[12px] font-medium text-center">
-                                <div>
-                                    <button className="flex items-center justify-center text-blue-600 w-full h-full">
-                                        <img src={menu} alt="icon" />
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr
-                            className="  rounded-lg p-5 relative mb-2 "
-                        >
-                            <td className="p-2 w-[400px] ">
-                                <div className="flex items-center">
-                                    <img
-                                        className="w-[50px] h-[50px] rounded-lg mr-[10px]"
-                                        src={depositIcon}
-                                        alt="Item Image"
-                                    />
-                                    <div>
-                                        <p className="text-[12px] font-medium">Withdraw Funds</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td className="p-2 w-[300px] text-[12px] font-medium">
-                                7 Aug 2023
-
-                            </td>
-                            <td className="p-2 w-[300px] text-[12px] font-medium">N23,000</td>
-                            <td className="p-2 w-[150px] text-[12px] font-medium">
-                                <div className="flex items-center justify-center w-full h-full">
-                                    <p className="bg-[#FFA800] px-2 py-1 rounded-full text-center text-white text-[10px] font-medium">
-                                        Pending
-                                    </p>
-                                </div>
-                            </td>
-                            <td className=" p-2 w-[150px] relative text-[12px] font-medium text-center">
-                                <div>
-                                    <button className="flex items-center justify-center text-blue-600 w-full h-full">
-                                        <img src={menu} alt="icon" />
-                                    </button>
-                                </div>
-                            </td>
-                        </tr> */}
                     </tbody>
                 </table>
 
