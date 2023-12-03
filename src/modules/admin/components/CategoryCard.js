2c4b3bf99f8ecb6d74e606cb5cb5656264e0fbc0
f-deniyi <holarfemilekan049@gmail.com>
2023-12-03 09:55:07 -0800

    product details

--- src/modules/admin/components/CategoryCard.js
+++ src/modules/admin/components/CategoryCard.js
@@ -26,7 +26,7 @@ const CategoryCard = ({
           className={`${type !== "brand"
             ? "bg-brandPrimary"
             : "bg-white border border-1 border-[#828282] border-solid "
-            } h-[124px] w-124px] flex justify-center items-center mb-[15px] rounded-lg`}
+            } h-[124px] w-[124px] flex justify-center items-center mb-[15px] rounded-lg`}
         >
           <img
             src={type === "brand" ? image ?? brand : icon}
@@ -35,7 +35,7 @@ const CategoryCard = ({
           />
         </div>
         <p className="text-[15px] font-normal mb-2 text-center">{name}</p>
-        <p className="text-[15px] font-normal mb-2 bg-black rounded-full text-white text-[10px] font-medium py-2 px-3 text-center">{`${count}/products`}</p>
+        {/* <p className="text-[15px] font-normal mb-2 bg-black rounded-full text-white text-[10px] font-medium py-2 px-3 text-center">{`${count}/products`}</p> */}
       </div>
       <CategoryDetails
         category={categoryDetails}
--- src/modules/products/components/Details/index.js
+++ src/modules/products/components/Details/index.js
@@ -5,8 +5,11 @@ import { Carousel } from 'react-responsive-carousel'
 import product from '../../../../assets/images/product_details.png'
 import DeleteProduct from '../DeleteProduct';
 import useGetSingleProductQuery from '../../controllers/get_single_product';
+import { useLocation } from 'react-router-dom'
 
 const customIndicator = (onClickHandler, isSelected, index, label) => {
+
+
     const indicatorStyle = {
         width: '7px',
         height: '7px',
@@ -35,6 +38,8 @@ const customIndicator = (onClickHandler, isSelected, index, label) => {
 
 
 const ProductDetails = ({ productId, setProductId }) => {
+    let location = useLocation();
+    const isAdmin = location.pathname.includes("/admin");
 
     const { product } = useGetSingleProductQuery({
         productId,
@@ -96,21 +101,24 @@ const ProductDetails = ({ productId, setProductId }) => {
                                 <p className="text-[11px] text-[#696969] font-normal mr-2">Silverline Estate Lekki Lagos</p>
                             </div> */}
                         </div>
-                        <div className='flex gap-2'>
-                            <div className='cursor-pointer flex items-center justify-center bg-brandPrimary border-2 border-black h-[60px] w-[60px] rounded-full'>
-                                <img src={edit} alt='icon' />
-                            </div>
-                            <div className='cursor-pointer'
-                                onClick={() => {
-                                    document.getElementById('product_details').close()
-                                    document.getElementById('delete_product').showModal()
+                        {
+                            !isAdmin && <div className='flex gap-2'>
+                                <div className='cursor-pointer flex items-center justify-center bg-brandPrimary border-2 border-black h-[60px] w-[60px] rounded-full'>
+                                    <img src={edit} alt='icon' />
+                                </div>
+                                <div className='cursor-pointer'
+                                    onClick={() => {
+                                        document.getElementById('product_details').close()
+                                        document.getElementById('delete_product').showModal()
 
-                                }}
-                            >
-                                <img src={deleteIcon} alt='icon' />
-                            </div>
+                                    }}
+                                >
+                                    <img src={deleteIcon} alt='icon' />
+                                </div>
 
-                        </div>
+                            </div>
+                        }
+
 
                     </div>
                     <div className='mb-3 '>
@@ -118,21 +126,31 @@ const ProductDetails = ({ productId, setProductId }) => {
                         <div className="flex w-full">
                             {product?.attributes?.map((attribute, index) => (
                                 <React.Fragment key={index}>
-                                    <div className="grid mr-3 flex-grow">
+                                    <div className="flex flex-col mr-3 flex-grow">
                                         <p className={`text-[#696969] text-[15px] font-normal mb-[10px] capitalize ${attribute?.name !== 'colour' ? 'text-center' : ""}`} >{attribute?.name}</p>
                                         {
-                                            attribute?.name === 'colour' ? (
+                                            /color|colour/i.test(attribute?.name) ? (
                                                 <div className='flex gap-1'>
                                                     {attribute?.values?.map((el, subIndex) => (
                                                         <div className='h-[36px] rounded-full w-[36px] bg-[#FF0000]' style={{ background: el?.value }} key={subIndex} />
                                                     ))}
                                                 </div>
                                             ) : (
-                                                <>
+                                                <div className='flex flex-wrap'>
                                                     {attribute?.values?.map((el, subIndex) => (
-                                                        <h3 className='text-[18px] font-medium text-center' key={subIndex}>{el.value}</h3>
+                                                        <>
+                                                            <h3 className='text-[18px] font-medium text-center' key={subIndex}>{el.value}</h3>
+                                                            <>
+                                                                {
+                                                                    subIndex !== attribute?.values.length - 1 && <span className='px-1'>,</span>
+
+                                                                }
+                                                            </>
+
+                                                        </>
+
                                                     ))}
-                                                </>
+                                                </div>
                                             )
                                         }
                                     </div>
