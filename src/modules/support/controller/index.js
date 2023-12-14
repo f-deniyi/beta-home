40e615a05035f21f09a1109070d0e439e2a66241
f-deniyi <holarfemilekan049@gmail.com>
2023-12-14 15:45:05 -0800

    support

---
+++ src/modules/support/controller/index.js
@@ -0,0 +1,23 @@
+import { toast } from 'react-toastify'
+import { useMutation } from 'react-query';
+import Axios from '../../../constants/api_management/MyHttpHelper';
+
+const UseSupportWithoutAuth = () => {
+    const { mutate: createTicket, isLoading, isSuccess } = useMutation(async (data) => {
+        try {
+            const response = await Axios.post('/support/create', data);
+            return response.data;
+        } catch (error) {
+            console.log(error.response.data?.message);
+            toast.error(error?.response?.data?.message)
+            throw new Error(`Sorry: ${error.response.data.message}`);
+        }
+    }
+    )
+    return {
+        createTicket,
+        isLoading,
+        isSuccess,
+    };
+};
+export default UseSupportWithoutAuth;
